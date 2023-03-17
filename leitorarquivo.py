eac76815a14d72338b3f7235d6f79639a84dd32d
Guilherme Pasqual de Toni <2020307170@FARROUPILHA.IFRS.EDU.BR>
2023-03-17 15:05:56 -0300

    Função principal do programa em novo arquivo

--- leitorarquivo.py
+++ leitorarquivo.py
@@ -5,8 +5,3 @@ class LeitorArquivo:
    
     def getValores(self):
         return self.valores
-
-if __name__ == '__main__':
-    leitor = LeitorArquivo("data.txt")
-    listaValores = leitor.getValores()
-    print(listaValores)
